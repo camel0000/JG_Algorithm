0a70aa5f3a4f4ac36a8648839d8bf58efefe3d4e
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-16 16:38:57 +0900

    [Gold IV] Title: PPAP, Time: 400 ms, Memory: 45144 KB -BaekjoonHub

--- 백준/Gold/16120. PPAP/PPAP.py
+++ 백준/Gold/16120. PPAP/PPAP.py
@@ -10,7 +10,6 @@ for i in range(len(br)):
     stack.append(br[i])
     
     if len(stack) >= 4 and stack[-1] == 'P':
-        # print(i)
         if stack[-1] == 'P':
             if stack[-2] == 'A':
                 if stack[-3] == 'P':
@@ -18,10 +17,7 @@ for i in range(len(br)):
                         stack.pop()
                         stack.pop()
                         stack.pop()
-                        stack.pop()
-                        stack.append('P')
 
-# print(stack)
 if len(stack) == 1 and stack[0] == 'P':
     print('PPAP')
 else:
