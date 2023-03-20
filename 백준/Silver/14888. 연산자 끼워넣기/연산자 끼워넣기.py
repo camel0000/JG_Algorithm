0b748f636749d3f4bd3b3e013d49415b09311b08
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-20 20:39:40 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -2,32 +2,33 @@
 
 import sys
 input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
 
-def dfs(depth, total, plus, minus, multiply, divide):   # total 파라미터는 A 배열에 있는 수들을 누적(by 재귀) 계산하기 위한 것
+def dfs(depth, total, plus, minus, multi, div):
     global _max, _min
     
     if depth == N:
-        _max = max(total, _max)
-        _min = min(total, _min)
-    
-    if plus:        # plus 변수가 0이 아니면(+연산자가 남아있을 경우)
-        dfs(depth + 1, total + A[depth], plus - 1, minus, multiply, divide)
+        _max = max(_max, total)
+        _min = min(_min, total)
+        
+    if plus:
+        dfs(depth + 1, total + A[depth], plus - 1, minus, multi, div)
     if minus:
-        dfs(depth + 1, total - A[depth], plus, minus - 1, multiply, divide)
-    if multiply:
-        dfs(depth + 1, total * A[depth], plus, minus, multiply - 1, divide)
-    if divide:
-        dfs(depth + 1, int(total / A[depth]), plus, minus, multiply, divide - 1)    # 문제 조건대로 나눗셈 설계
+        dfs(depth + 1, total - A[depth], plus, minus - 1, multi, div)
+    if multi:
+        dfs(depth + 1, total * A[depth], plus, minus, multi - 1, div)
+    if div:
+        dfs(depth + 1, int(total / A[depth]), plus, minus, multi, div - 1)
 
 
 N = int(input())
 A = list(map(int, input().split()))
-plus, minus, multiply, divide = map(int, input().split())
+plus, minus, multi, div = map(int, input().split())
 
-visited = [0] * N
-_max = -1e9
-_min = 1e9
+visited = [0] * (N + 1)
 
-dfs(1, A[0], plus, minus, multiply, divide)
+_max, _min = -1e9, 1e9
+
+dfs(1, A[0], plus, minus, multi, div)
 print(_max)
 print(_min)
