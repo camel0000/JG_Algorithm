45c83714e33df6f263e7b1367895957e312e3f0c
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-20 09:49:05 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34144 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,34 +1,30 @@
-# 2178 : 미로 탐색
+# 2178_04 : 미로 탐색
 
 import sys
 input = sys.stdin.readline
 from collections import deque
 
-N, M = map(int, input().split())
-miro = [list(map(int, input().rstrip())) for _ in range(N)]
-
-
-def miro_bfs(x, y):
-    queue = deque()
-    queue.append((x, y))
+def bfs():
+    q = deque()
+    q.append((0, 0))
     
-    dx = [-1, 1, 0, 0]
-    dy = [0, 0, -1, 1]
-    
-    while queue:
-        x, y = queue.popleft()
+    while q:
+        x, y = q.popleft()
         
         for i in range(4):
             nx = x + dx[i]
             ny = y + dy[i]
             
-            if nx < 0 or nx >= N or ny < 0 or ny >= M or (nx == 0 and ny == 0):  # 공간을 벗어나는 경우, 이동할 수 없는 칸인 경우
-                continue
-                
-            if miro[nx][ny] == 1:
-                miro[nx][ny] = miro[x][y] + 1
-                queue.append((nx, ny))
-    
-    return miro[N - 1][M - 1]
+            if 0 <= nx < N and 0 <= ny < M and maze[nx][ny] == 1:
+                maze[nx][ny] = maze[x][y] + 1
+                q.append((nx, ny))
 
-print(miro_bfs(0, 0))
+
+N, M = map(int, input().split())
+maze = [list(map(int, input().rstrip())) for _ in range(N)]
+
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+
+bfs()
+print(maze[N - 1][M - 1])
