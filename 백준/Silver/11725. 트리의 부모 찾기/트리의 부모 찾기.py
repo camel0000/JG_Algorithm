bca75958920464a233594cc3700caccebd6b797d
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-20 16:48:33 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 336 ms, Memory: 65060 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.py
@@ -0,0 +1,25 @@
+# 11725 : 트리의 부모 찾기
+
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**9)
+
+def dfs(v):
+    for i in graph[v]:
+        if visited[i] == 0:
+            visited[i] = v
+            dfs(i)
+
+
+N = int(input())
+graph = [[] for _ in range(N + 1)]
+for _ in range(N - 1):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+visited = [0] * (N + 1)
+
+dfs(1)
+for i in range(2, N + 1):
+    print(visited[i])
