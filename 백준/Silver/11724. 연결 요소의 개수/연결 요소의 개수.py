a69d0627ec266e7656eeb066846a453ac575293a
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-20 09:28:36 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 716 ms, Memory: 65220 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,13 +1,12 @@
-# 11724_02 : 연결 요소의 개수
-
-# bfs 풀이
+# 11724_03 : 연결 요소의 개수
 
 import sys
 input = sys.stdin.readline
 from collections import deque
 
 def bfs(v):
-    q = deque([v])
+    q = deque()
+    q.append(v)
     visited[v] = 1
     
     while q:
@@ -21,18 +20,17 @@ def bfs(v):
 
 N, M = map(int, input().split())
 graph = [[] for _ in range(N + 1)]
-
 for _ in range(M):
-    a, b = map(int ,input().split())
-    graph[a].append(b)
-    graph[b].append(a)
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
 
-visited = [0] * (N + 1)
-rs = 0
+visited = [0] * (N + 1)    
+cnt = 0
 
 for i in range(1, N + 1):
     if visited[i] == 0:
         bfs(i)
-        rs += 1
-    
-print(rs)
+        cnt += 1
+
+print(cnt)
