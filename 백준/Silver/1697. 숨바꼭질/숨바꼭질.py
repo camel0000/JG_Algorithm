78be565f1e11b52a226672b9fe14beefd2997f4b
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-23 11:01:20 +0900

    [Silver I] Title: 숨바꼭질, Time: 116 ms, Memory: 35108 KB -BaekjoonHub

---
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -0,0 +1,36 @@
+# 1697 : 숨바꼭질
+
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+def bfs(n):
+    q = deque()
+    q.append(n)
+    
+    
+    # front = X + 1
+    # back = X - 1
+    # tele = 2 * X
+    
+    while q:
+        X = q.popleft()
+        dX = [X - 1, X + 1, 2 * X]
+        
+        if X == K:
+            return visited[X]
+        
+        for i in range(3):
+            nX = dX[i]
+            
+            if 0 <= nX < 100001 and visited[nX] == 0:
+                visited[nX] = visited[X] + 1
+                q.append(nX)
+                
+    return visited[nX]
+        
+
+N, K = map(int, input().split())
+visited = [0] * (100001)
+
+print(bfs(N))
