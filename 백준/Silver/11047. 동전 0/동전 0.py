e2533929c65698886f561a8da0a79a2bc0657455
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-24 13:55:01 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.py
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -1,10 +1,19 @@
-N, K = map(int, input().split())
+# 11047 : 동전 0
 
-arr = [int(input()) for _ in range(N)]
+import sys
+input = sys.stdin.readline
+
+N, K = map(int, input().split())
+coins = [int(input()) for _ in range(N)]
+
+coins.sort(reverse=True)
+
 cnt = 0
 
-for x in reversed(range(N)) :
-    cnt += (K // arr[x])
-    K = K % arr[x] 
-        
+for coin in coins:
+    if coin > K:
+        continue
+    cnt += (K // coin)
+    K %= coin
+    
 print(cnt)
