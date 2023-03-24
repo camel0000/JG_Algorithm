7e98cf0a9d588fb7d166ed513954f575ae382514
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-24 13:37:43 +0900

    [Silver III] Title: 01타일, Time: 324 ms, Memory: 69788 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,14 @@
+# 1904 : 01타일
+
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+
+d = [0] * 1000001
+d[1], d[2] = 1, 2
+
+for i in range(3, N + 1):
+    d[i] = (d[i - 1] + d[i - 2]) % 15746
+
+print(d[N])
