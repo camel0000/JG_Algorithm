0a0a423c0c2839b32db5ab1149d29b892638cfb8
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-26 14:00:49 +0900

    [Gold V] Title: 벼락치기, Time: 516 ms, Memory: 52932 KB -BaekjoonHub

--- 백준/Gold/14728. 벼락치기/벼락치기.py
+++ 백준/Gold/14728. 벼락치기/벼락치기.py
@@ -1,4 +1,4 @@
-# 14728 : 벼락치기
+# 14728_02 : 벼락치기
 
 import sys
 input = sys.stdin.readline
@@ -8,11 +8,11 @@ N, T = map(int, input().split())
 dp = [[0] * (T + 1) for _ in range(N + 1)]
 
 for i in range(1, N + 1):
-    expected_T, point = map(int, input().split())
+    ex_T, point = map(int, input().split())
     
     for j in range(1, T + 1):
-        if j - expected_T >= 0:
-            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - expected_T] + point)
+        if j - ex_T >= 0:
+            dp[i][j] = max(dp[i - 1][j], dp[i - 1][j - ex_T] + point)
         else:
             dp[i][j] = dp[i - 1][j]
 
