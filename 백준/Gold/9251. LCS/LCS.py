ade4c2003ff856acf6900cb102c33076fe42e340
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-29 00:31:44 +0900

    [Gold V] Title: LCS, Time: 520 ms, Memory: 55712 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -1,19 +1,18 @@
-# 9251 : LCS
+# 9251_02 : LCS
 
 import sys
 input = sys.stdin.readline
-from itertools import combinations
 
 str1 = list(input().rstrip())
 str2 = list(input().rstrip())
 
 h, w = len(str1), len(str2)
-table = [[0] * (w + 1) for _ in range(h + 1)]
+dp = [[0] * (w + 1) for _ in range(h + 1)]
 
 for i in range(1, h + 1):
     for j in range(1, w + 1):
         if str1[i - 1] == str2[j - 1]:
-            table[i][j] = table[i - 1][j - 1] + 1
+            dp[i][j] = dp[i - 1][j - 1] + 1
         else:
-            table[i][j] = max(table[i][j - 1], table[i - 1][j])
-print(table[-1][-1])
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+print(dp[h][w])
