70ae445bce5f3106618ed487e22950fa1dbb5a04
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-28 22:08:23 +0900

    [Silver III] Title: Four Squares, Time: 248 ms, Memory: 115632 KB -BaekjoonHub

--- 백준/Silver/17626. Four Squares/Four Squares.py
+++ 백준/Silver/17626. Four Squares/Four Squares.py
@@ -2,22 +2,14 @@
 
 import sys
 input = sys.stdin.readline
-from math import sqrt
-from itertools import combinations_with_replacement
 
+# DP
 N = int(input())
-square_num_1 = [i*i for i in range(1, int(sqrt(N)) + 1)]
-square_num_2 = [sum(k) for k in combinations_with_replacement(square_num_1, 2)]
-
-if N in square_num_1:
-    print(1)
-    exit()
-elif N in square_num_2:
-    print(2)
-    exit()
-else:
-    for sq in square_num_1:
-        if N - sq in square_num_2:
-            print(3)
-            exit()
-print(4)
+square_num = [i*i for i in range(1, 224)]
+dp = [0 for _ in range(N + 1)]
+for i in range(1, N + 1):
+    if i in square_num:
+        dp[i] = 1
+    else:
+        dp[i] = min([dp[i - j] for j in square_num if i - j > 0]) + 1
+print(dp[N])
