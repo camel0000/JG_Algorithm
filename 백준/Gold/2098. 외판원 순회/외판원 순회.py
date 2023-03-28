9bb786f8943d95ce5db665b17f7c592a8c83a803
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-28 15:08:40 +0900

    [Gold I] Title: 외판원 순회, Time: 1284 ms, Memory: 42320 KB -BaekjoonHub

---
+++ 백준/Gold/2098. 외판원 순회/외판원 순회.py
@@ -0,0 +1,36 @@
+def isIn(i, A):
+    return (A & (1 << (i - 1))) != 0
+
+def diff(A, j):
+    t = 1 << (j - 1)
+    return A & (~t)
+
+def minimum(w, D, i, A):
+    min_val = INF
+    for j in range(1, n):
+        if isIn(j, A):
+            m = w[i][j] + D[j][diff(A, j)]
+            min_val = min(min_val, m)
+    return min_val
+
+def travel(n, w):
+    size = 2 ** (n - 1)
+    D = [[0] * size for _ in range(n)]
+    for i in range(1, n):
+        D[i][0] = w[i][0]
+    for k in range(1, n - 1):
+        for A in range(1, size - 1):
+            if bin(A).count('1') == k:
+                for i in range(1, n):
+                    if not isIn(i, A):
+                        D[i][A] = minimum(w, D, i, A)
+    A = size - 1
+    return minimum(w, D, 0, A)
+
+INF = int(1e9)
+n = int(input())
+w = []
+for _ in range(n):
+    w.append([INF if m == 0 else m for m in list(map(int, input().split()))])
+
+print(travel(n, w))
