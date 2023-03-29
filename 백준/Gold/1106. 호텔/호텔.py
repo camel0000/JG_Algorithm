81a07fbc8845fb7d8f6e75084b1923607d6d8545
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-03-30 00:16:48 +0900

    [Gold V] Title: 호텔, Time: 1060 ms, Memory: 110180 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.py
+++ 백준/Gold/1106. 호텔/호텔.py
@@ -1,24 +1,21 @@
-# 1106_02 : 호텔
+# 1106_03 : 호텔
 
 import sys
 input = sys.stdin.readline
 
 C, N = map(int, input().split())
-dp = [[sys.maxsize] * (C + 1) for _ in range(N + 1)]
+dp = [[0] * (100 * C + 1) for _ in range(N + 1)]
 
 for i in range(1, N + 1):
     cost, person = map(int, input().split())
         
-    for j in range(1, C + 1): 
-        k = 0
-        dp[i][j] = dp[i - 1][j]
-        
-        while 1:
-            if j - person * k > 0:
-                dp[i][j] = min(dp[i][j], dp[i - 1][j - person * k] + cost * k)
-            else:
-                dp[i][j] = min(dp[i][j], cost * k)
-                break
-            k += 1
+    for j in range(1, 100 * C + 1): 
+        if j - cost >= 0:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - cost] + person)
+        else:
+            dp[i][j] = dp[i - 1][j]
 
-print(dp[-1][-1])
+for j in range(1, 100 * C + 1):
+    if dp[N][j] >= C:
+        print(j)
+        break
