19780ada53c0c5fe9155c923047b2f1e2f374144
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-04-17 10:37:17 +0900

    [Silver II] Title: A → B, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,25 @@
+# 23-04-17
+# 16953 : A -> B
+
+import sys
+input = sys.stdin.readline
+
+A, B = map(int, input().split())
+
+count = 0
+
+while A < B:    
+    if B % 10 == 1: # B의 마지막 숫자가 1일 경우
+        B = B // 10
+    elif B / 2 == B // 2:
+        B = B // 2
+    else:
+        print(-1)
+        exit()
+        
+    count += 1
+    
+if A == B:
+    print(count + 1)
+else:
+    print(-1)
