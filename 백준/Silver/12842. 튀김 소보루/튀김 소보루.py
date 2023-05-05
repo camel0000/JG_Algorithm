6f59e76f2d5f2ad1a362e3a48dbd1fbd6de5be69
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-05-06 00:01:15 +0900

    [Silver II] Title: 튀김 소보루, Time: 1428 ms, Memory: 37232 KB -BaekjoonHub

--- 백준/Silver/12842. 튀김 소보루/튀김 소보루.py
+++ 백준/Silver/12842. 튀김 소보루/튀김 소보루.py
@@ -2,11 +2,27 @@
 # 12842 : 튀김 소보루
 
 import sys
+import math
 input = sys.stdin.readline
 
 n, s = map(int, input().split())
 m = int(input())
 times = [int(input()) for _ in range(m)]
+
+_lcm = 1
+for t in times:
+    _lcm = math.lcm(_lcm, t)
+    
+total_per_cycle = 0
+for i in range(m):
+    total_per_cycle += _lcm // times[i]
+
+quot = (n - s) // total_per_cycle
+n -= quot * total_per_cycle
+
+if n == s:
+    print(m)
+    exit()
 
 total = 0
 for i in range(1000000):
