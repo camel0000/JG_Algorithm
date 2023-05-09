1442ca006f1d38e5c32bc72cdde0bdd5bb25b5af
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-05-10 00:38:42 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 88 ms, Memory: 33324 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.py
@@ -1,46 +1,49 @@
-# 23-04-03
+# 23-05-09
 # 4485 : 녹색 옷 입은 애가 젤다지?
 
 import sys
 input = sys.stdin.readline
-from collections import deque
+import heapq
+INF = int(1e9)
 
 dx = [-1, 1, 0, 0]
 dy = [0, 0, -1, 1]
 
-def bfs():
-    q = deque()
-    q.append((0, 0))
-    dp[0][0] = arr[0][0]
-    visited[0][0] = 1
+def dijkstra():
+    q = []
+    heapq.heappush(q, (maps[0][0], 0, 0))
+    dist[0][0] = 0
     
     while q:
-        x, y = q.popleft()
+        cost, x, y = heapq.heappop(q)
+        
+        if x == N - 1 and y == N - 1:
+            print("Problem " + str(count) + ": " + str(cost))
+            break
         
         for i in range(4):
-            nx, ny = x + dx[i], y + dy[i]
-
+            nx = x + dx[i]
+            ny = y + dy[i]
+            
             if 0 <= nx < N and 0 <= ny < N:
-                if visited[nx][ny]:
-                    if dp[nx][ny] > dp[x][y] + arr[nx][ny]:
-                        q.append((nx, ny))
-                        dp[nx][ny] = min(dp[nx][ny], dp[x][y] + arr[nx][ny])
-                else:
-                    q.append((nx, ny))
-                    dp[nx][ny] = dp[x][y] + arr[nx][ny]
-                    visited[nx][ny] = 1
+                ncost = cost + maps[nx][ny]
+                
+                if ncost < dist[nx][ny]:
+                    dist[nx][ny] = ncost
+                    heapq.heappush(q, (ncost, nx, ny))
 
 
-idx = 0
+count = 1
+
 while 1:
-    idx += 1
     N = int(input())
-    if N == 0:
-        break
     
-    arr = [list(map(int, input().split())) for _ in range(N)]
-    visited = [[0 for _ in range(N)] for _ in range(N)]
-    dp = [[0 for _ in range(N)] for _ in range(N)]
-
-    bfs()
-    print("Problem " + str(idx) + ": " + str(dp[N - 1][N - 1]))
+    if N == 0:
+        exit()
+    
+    maps = [list(map(int, input().split())) for _ in range(N)]
+    dist = [[INF] * (N) for _ in range(N)]
+    
+    
+    dijkstra()
+    count += 1
