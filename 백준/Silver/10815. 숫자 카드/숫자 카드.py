bfd5860fe6e3b6e404271039e5091fe48cf23126
Mingi Cho <82951045+camel0000@users.noreply.github.com>
2023-07-16 19:29:54 +0900

    [Silver V] Title: 숫자 카드, Time: 676 ms, Memory: 125204 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -3,29 +3,14 @@
 import sys
 input = sys.stdin.readline
 
-def binarySearch(start, end, target):
-    check = 0
-    
-    while start <= end:
-        mid = (start + end) // 2
-        
-        if target == cards[mid]:
-            check = 1
-            break
-        elif target > cards[mid]:
-            start = mid + 1
-        else:
-            end = mid - 1
-            
-    if check == 1:
-        return 1
-    else:
-        return 0
-
 N = int(input())
-cards = sorted(list(map(int, input().split())))
-M = int(input())
-_list = list(map(int, input().split()))
+cards = set(map(int, input().split()))
 
-for i in _list:
-    print(binarySearch(0, len(cards) - 1, i))
+M = int(input())
+numbers = list(map(int, input().split()))
+
+for n in numbers:
+    if n in cards:
+        print(1, end=' ')
+    else:
+        print(0, end=' ')
